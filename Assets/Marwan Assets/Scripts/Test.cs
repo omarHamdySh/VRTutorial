943f909c7f241e0b800f7c728cc4c00229087c68
Omar Hamdy <omar.h.shafeek@gmail.com>
2019-07-15 14:49:45 +0200

    Conflict creation

--- Assets/Marwan Assets/Scripts/Test.cs
+++ Assets/Marwan Assets/Scripts/Test.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
 
-    bool omarFlag = true;
-
+    bool omarFlag = false;
     // Start is called before the first frame update
     void Start()
     {
