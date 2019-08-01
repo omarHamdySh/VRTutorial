1e2bdf42c7a7f6d696187c58a74a1723435d2541
Marwan Osama EL-Safy <MarwanOsama1997@gmail.com>
2019-08-01 13:10:54 +0200

    Grenade animation disassembly

---
+++ Assets/Marwan Assets/Scripts/ObjectController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    private Vector2 touchAxis;
+
+    public void SetTouchAxis(Vector2 data)
+    {
+        touchAxis = data;
+    }
+
+    private void Update()
+    {
+        if (touchAxis != new Vector2(500, 500))
+        {
+            ////// Rotation 
+            if (transform.rotation.x < 180 && transform.rotation.y < 180 && transform.rotation.z < 180)
+            {
+
+                if (touchAxis.x > .7)
+                {
+                    transform.Rotate(0, -1f, 0, 0);
+
+
+                }
+            }
+            if (transform.localScale.x > -180 && transform.localScale.y > -180 && transform.localScale.z > -180)
+            {
+                if (touchAxis.x < -.7)
+                {
+                    transform.Rotate(0, 1f, 0, 0);
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Marwan Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+
+    public Animator anim;
+    bool isplay = true;
+    public GameObject showInforamtionObject;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Play_animator()
+    {
+        Debug.Log("play animation for motor");
+        if (isplay)
+        {
+            print("conditation if");
+            anim.enabled = true;
+            anim.Play("MK2_Animated_disassembly_open");
+            anim.SetTrigger("x");
+            StartCoroutine(ShowInformation());
+        }
+        else
+        {
+            anim.Play("TESTANIM");
+            anim.enabled = true;
+        }
+
+        isplay = !isplay;
+    }
+
+    IEnumerator ShowInformation()
+    {
+        yield return new WaitForSeconds(7f);
+        showInforamtionObject.SetActive(true);
+    }
+
+}
