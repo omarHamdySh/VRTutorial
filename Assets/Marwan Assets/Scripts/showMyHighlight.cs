90a2098ab80d111c2841bd723e31d8b85ac67660
Marwan Osama EL-Safy <MarwanOsama1997@gmail.com>
2019-08-06 15:03:12 +0200

    Ui hidden pointer until aim the Ui

---
+++ Assets/Marwan Assets/Scripts/showMyHighlight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showMyHighlight : MonoBehaviour
+{
+    public GameObject HighlightGameObject;
+    // Use this for initialization
+    void Start()
+    {
+        HighlightGameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void EnableHighlight()
+    {
+        HighlightGameObject.SetActive(true);
+
+    }
+    public void DisableHighlight()
+    {
+
+        HighlightGameObject.SetActive(false);
+    }
+}
