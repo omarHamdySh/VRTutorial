94897e381e946e4031c93a96d4854632b41902da
Omar Hamdy <omar.h.shafeek@gmail.com>
2019-08-07 14:32:48 +0200

    New Generic Script (Game Scene Manager "SceneMapping Script")

--- Assets/Documentation Assets/Omar/Documentation Gifs/Readme.txt
+++
@@ -1,7 +0,0 @@
-Documentation of the first week:
-
-Omar Hamdy and Marawan Osama
-
-Version of unity: 2018.3.12
-Project name: VRTutorial
-Github is used;
---
+++ Assets/SceneMapping.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum ScenesNames {
+    Home,
+    Shooter,
+    Assembly
+}
+
+[System.Serializable]
+public class SceneMap {
+    [SerializeField]
+    public ScenesNames sceneIndex;
+}
+public class SceneMapping : MonoBehaviour
+{
+    public static SceneMapping _Instance;
+    private void Awake() {
+        _Instance = this;
+        DontDestroyOnLoad(this);
+    }
+    
+    [SerializeField]
+    List<SceneMap> scenesMaps = new List<SceneMap>();
+
+    public void changeScene(ScenesNames sceneName) {
+
+        SceneManager.LoadScene(sceneName.ToString());
+    }
+}
